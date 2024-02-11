ba5e85162e95261727c508f9856925a5855716ad
code-Abhishek <abhishek.worx.here@gmail.com>
2024-02-11 17:34:10 -0500

    Web application working with prediction

--- app.py
+++ app.py
@@ -22,6 +22,14 @@ def predict_api():
     print(output[0])
     return jsonify(output[0])
 
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=scalar.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=(regmodel.predict(final_input)[0]*100000)
+    return render_template("home.html",prediction_text="You can get the house for a predicted price of ${}".format(output))
+
+
 if __name__=="__main__":
     app.run(debug=True)
-    
